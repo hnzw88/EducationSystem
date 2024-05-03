c2307363b79a521cd7c5289b346b3d0234489e79
miki-y44 <mikiyamazaki0404@gmail.com>
2024-05-03 17:46:44 +0900

    yamazaki_firstcommit

---
+++ database/migrations/2024_04_30_110936_create_delivery_times_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_times', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('curriculums_id')->length(10);
+            $table->dateTime('delivery_from')->nullable(false);
+            $table->dateTime('delivery_to')->nullable(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_times');
+    }
+};
